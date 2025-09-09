b7f283404d64e10a05275293692eb2737098c5cb
Jean090504 <jc090504@gmail.com>
2025-09-09 16:40:10 -0300

    Início de código

--- src/MaquinaApp.java
+++ src/MaquinaApp.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class MaquinaApp{
 
-public class MaquinaApp {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Programa...");
+
+        MaquinaDeVendas maquina = new MaquinaDeVendas();
+
+        maquina.EntradaDeBebida();
+
+    }
+
+
+
+
 }
